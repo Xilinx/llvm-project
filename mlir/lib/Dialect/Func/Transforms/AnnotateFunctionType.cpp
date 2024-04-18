b92bee11f65897a2028441b422bc981dcd862a9c
Jose Lopes <jose.lopes@amd.com>
2024-04-18 13:04:00 +0100

    Address comments

--- mlir/lib/Dialect/Func/Transforms/AnnotateFunctionType.cpp
+++ mlir/lib/Dialect/Func/Transforms/AnnotateFunctionType.cpp
@@ -20,26 +20,29 @@
 
 using namespace mlir;
 
-namespace mlir {
-#define GEN_PASS_DEF_ANNOTATEINPUTTYPES
+namespace mlir::func {
+#define GEN_PASS_DECL_ANNOTATEFUNCTIONTYPE
+#define GEN_PASS_DEF_ANNOTATEFUNCTIONTYPE
 #include "mlir/Dialect/Func/Transforms/Passes.h.inc"
-} // namespace mlir
+} // namespace mlir::func
 
 namespace {
-struct AnnotateInputTypesPass
-    : public impl::AnnotateInputTypesBase<AnnotateInputTypesPass> {
+struct AnnotateFunctionTypePass
+    : public mlir::func::impl::AnnotateFunctionTypeBase<
+          AnnotateFunctionTypePass> {
 
   void runOnOperation() override {
     func::FuncOp func = getOperation();
     auto inputs = func.getArgumentTypes();
+    auto results = func.getResultTypes();
 
     for (const auto [argNum, type] : llvm::enumerate(inputs)) {
       func.setArgAttr(argNum, "func.orig_type", TypeAttr::get(type));
     }
+
+    for (const auto [resultNum, type] : llvm::enumerate(results)) {
+      func.setResultAttr(resultNum, "func.orig_type", TypeAttr::get(type));
+    }
   }
 };
 } // namespace
-
-std::unique_ptr<Pass> mlir::func::createAnnotateInputTypesPass() {
-  return std::make_unique<AnnotateInputTypesPass>();
-}
