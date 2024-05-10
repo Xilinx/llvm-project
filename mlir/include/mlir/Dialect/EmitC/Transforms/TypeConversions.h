5803f4867f0e200171ee53e1ded7cd4343e18808
Corentin Ferry <corentin.ferry@amd.com>
2024-05-10 12:47:03 +0100

    Add type converter

---
+++ mlir/include/mlir/Dialect/EmitC/Transforms/TypeConversions.h
@@ -0,0 +1,11 @@
+//===- TypeConversions.h - Convert signless types into C/C++ types -------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Transforms/DialectConversion.h"
+
+void populateEmitCSizeTypeConversionPatterns(mlir::TypeConverter &converter);
---
+++ mlir/lib/Dialect/EmitC/Transforms/TypeConversions.cpp
@@ -0,0 +1,18 @@
+//===- TypeConversions.cpp - Convert signless types into C/C++ types ------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/EmitC/Transforms/TypeConversions.h"
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
+#include "mlir/IR/BuiltinTypes.h"
+
+using namespace mlir;
+
+void populateEmitCSizeTypeConversionPatterns(TypeConverter &converter) {
+  converter.addConversion(
+      [](IndexType type) { return emitc::UnsignedSizeType(); });
+}
