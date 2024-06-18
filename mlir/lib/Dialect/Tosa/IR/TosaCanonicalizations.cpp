502bef0d268dc630c2e90044874c3c8169670ef0
Corentin Ferry <corentin.ferry@amd.com>
2024-06-18 16:47:46 +0100

    Fix TOSA cast constant op folding

--- mlir/lib/Dialect/Tosa/IR/TosaCanonicalizations.cpp
+++ mlir/lib/Dialect/Tosa/IR/TosaCanonicalizations.cpp
@@ -888,7 +888,7 @@ OpFoldResult CastOp::fold(FoldAdaptor adaptor) {
           llvm::cast<IntegerType>(outETy).getIntOrFloatBitWidth(), unsign);
       auto floatVal = operand.getSplatValue<APFloat>();
       bool exact;
-      floatVal.convertToInteger(intVal, llvm::RoundingMode::TowardZero, &exact);
+      floatVal.convertToInteger(intVal, llvm::RoundingMode::NearestTiesToEven, &exact);
       return SplatElementsAttr::get(outTy, intVal);
     }
 
