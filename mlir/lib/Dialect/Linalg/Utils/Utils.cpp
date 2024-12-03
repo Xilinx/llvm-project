79ef42aef43f70d7d37afc78c22cfddd14db7d2e
Matthias Gehre <matthias.gehre@amd.com>
2024-12-03 14:43:35 +0100

    Fix comment

--- mlir/lib/Dialect/Linalg/Utils/Utils.cpp
+++ mlir/lib/Dialect/Linalg/Utils/Utils.cpp
@@ -618,8 +618,8 @@ computeSliceParameters(OpBuilder &builder, Location loc, Value valueToTile,
     LLVM_DEBUG(llvm::dbgs() << "computeSliceParameters: for dim#" << r);
     auto m = map.getSubMap({r});
     // The offset & size computation below only handles the case when
-    // the map is monotonic, i.e. the min and max values are attained at the
-    // lower and upper bounds of the iteration domain.
+    // the map is monotonically increasing, i.e. the min and max values are
+    // attained at the lower and upper bounds of the iteration domain.
     if (!isTiled(m, tileSizes) || !m.isComponentWiseMonotonicallyIncreasing()) {
       sliceParams.offsets.push_back(builder.getIndexAttr(0));
       OpFoldResult dim = createFoldedDimOp(builder, loc, valueToTile, r);
