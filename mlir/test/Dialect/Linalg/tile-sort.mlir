f1b74132eec6b07d35ce3948191a3ab1797df428
Corentin Ferry <corentin.ferry@amd.com>
2025-05-05 05:02:24 -0600

    Remove reverse worklist test

--- mlir/test/Dialect/Linalg/tile-sort.mlir
+++ mlir/test/Dialect/Linalg/tile-sort.mlir
@@ -22,28 +22,3 @@ module attributes {transform.with_named_sequence} {
     transform.yield
   }
 }
-
-// -----
-
-func.func @tile_in_reverse_op_operand_order(%arg: tensor<256xf32>) -> tensor<256xf32> {
-  %empty = tensor.empty() : tensor<256xf32>
-  %0 = linalg.ceil ins(%arg: tensor<256xf32>) outs(%empty: tensor<256xf32>) -> tensor<256xf32>
-  %empty1 = tensor.empty() : tensor<256xf32>
-  %1 = linalg.negf ins(%0 : tensor<256xf32>) outs(%empty1: tensor<256xf32>) -> tensor<256xf32>
-  %empty2 = tensor.empty() : tensor<256xf32>
-  %2 = linalg.powf {tile} ins(%0, %1: tensor<256xf32>, tensor<256xf32>) outs(%empty2: tensor<256xf32>) -> tensor<256xf32>
-
-  // CHECK: worklist: producer is %{{.*}} = linalg.negf
-  // CHECK: worklist: producer is %{{.*}} = linalg.ceil
-  // CHECK: worklist: producer is %{{.*}} = linalg.ceil
-
-  return %2 : tensor<256xf32>
-}
-
-module attributes {transform.with_named_sequence} {
-  transform.named_sequence @__transform_main(%arg1: !transform.any_op {transform.readonly}) {
-    %0 = transform.structured.match ops{["linalg.powf"]} attributes {"tile"} in %arg1 : (!transform.any_op) -> !transform.any_op
-    %1, %loops = transform.test.fuse_and_yield %0 [32] reverse_worklist true : (!transform.any_op) -> (!transform.any_op, !transform.any_op)
-    transform.yield
-  }
-}
--- mlir/test/lib/Interfaces/TilingInterface/TestTilingInterfaceTransformOps.cpp
+++ mlir/test/lib/Interfaces/TilingInterface/TestTilingInterfaceTransformOps.cpp
@@ -54,11 +54,12 @@ static llvm::SmallDenseSet<Operation *> collectTiledAndFusedOps(Operation *op) {
 /// Apply a tile and fuse transformation to all payload ops and store both the
 /// tiled operation as well as the created tile loops.
 template <typename Range>
-static LogicalResult applyTileAndFuseToAll(
-    RewriterBase &rewriter, Operation *transformOp, Range &&payloadOps,
-    unsigned numLoops, ArrayRef<OpFoldResult> tileSizes,
-    ArrayRef<int64_t> interchange, bool useForall, bool reverseWorkList,
-    TransformResults &transformResults) {
+static LogicalResult
+applyTileAndFuseToAll(RewriterBase &rewriter, Operation *transformOp,
+                      Range &&payloadOps, unsigned numLoops,
+                      ArrayRef<OpFoldResult> tileSizes,
+                      ArrayRef<int64_t> interchange, bool useForall,
+                      TransformResults &transformResults) {
   SmallVector<Operation *> tiledOps;
   SmallVector<SmallVector<Operation *>> loopOps(numLoops);
 
@@ -86,13 +87,6 @@ static LogicalResult applyTileAndFuseToAll(
     }
 
     scf::SCFTileAndFuseOptions tileAndFuseOptions;
-    if (reverseWorkList) {
-      tileAndFuseOptions.setWorklistInsertFn(
-          [](tensor::ExtractSliceOp op,
-             std::deque<tensor::ExtractSliceOp> &worklist) {
-            worklist.push_front(op);
-          });
-    }
     tileAndFuseOptions.setTilingOptions(tilingOptions);
 
     scf::SCFTileAndFuseOptions::ControlFnTy controlFn =
@@ -163,7 +157,7 @@ transform::TestFuseAndYieldOp::apply(TransformRewriter &rewriter,
   LogicalResult result = applyTileAndFuseToAll(
       rewriter, getOperation(), state.getPayloadOps(getTarget()),
       tileSizes.size() - llvm::count(tileSizes, 0), tileSizesOfr,
-      tileInterchange, getUseForall(), getReverseWorklist(), transformResults);
+      tileInterchange, getUseForall(), transformResults);
   return failed(result) ? DiagnosedSilenceableFailure::definiteFailure()
                         : DiagnosedSilenceableFailure::success();
 }
